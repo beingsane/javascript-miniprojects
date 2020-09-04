d7383c733a5eebb4e25e679265406c827e09e2c3
Umang Thadani <64704318+thadaniumang@users.noreply.github.com>
2020-09-05 00:43:54 +0530

    Switch-case break statement bug

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -13,17 +13,28 @@ let tick = () => {
 
     switch(month){
         case 0: month = 'January';
+            break;
         case 1: month = 'February';
+            break;
         case 2: month = 'March';
+            break;
         case 3: month = 'April';
+            break;
         case 4: month = 'May';
+            break;
         case 5: month = 'June';
+            break;
         case 6: month = 'July';
+            break;
         case 7: month = 'August';
+            break;
         case 8: month = 'September';
+            break;
         case 9: month = 'October';
+            break;
         case 10: month = 'November';
-        case 11: month = 'December'; 
+            break;
+        case 11: month = 'December';
     }
     let html = `<p><strong>Date:</strong> ${date} ${month}, ${year}</p>`;
     dateDiv.innerHTML = html;
